5fe869deda5aa426c93eb0849654f31b56547209
Manel Montilla <manel.montilla@gmail.com>
2022-01-04 13:30:58 +0100

    WIP

--- wruster/src/handlers.rs
+++ wruster/src/handlers.rs
@@ -6,6 +6,29 @@ use crate::http::headers::{Header, Headers};
 use crate::http::{Body, Request, Response, StatusCode};
 use crate::router::HttpHandler;
 
+
+/**
+Implents a handler that serves the files in a directory tree.
+
+# Examples
+
+```no_run
+use wruster::router;
+use wruster::handlers::serve_static;
+use wruster::http;
+use wruster::Server;
+
+let addr = "localhost:8085";
+let dir = "./";
+let routes = router::Router::new();
+let dir = dir.clone();
+let serve_dir: router::HttpHandler = Box::new(move |request| serve_static(&dir, &request));
+routes.add("/", http::HttpMethod::GET, serve_dir);
+let mut server = Server::new();
+server.run(addr, routes).unwrap();
+server.wait().unwrap();
+```
+*/
 pub fn serve_static(dir: &str, request: &Request) -> Response<'static> {
     let base_path: PathBuf = PathBuf::from(dir).canonicalize().unwrap();
     let mut uri = request.uri.as_str();
@@ -59,6 +82,34 @@ pub fn serve_static(dir: &str, request: &Request) -> Response<'static> {
     }
 }
 
+/**
+A middleware that uses the log to print the request and response to
+the standard output with INFO level.
+
+# Examples
+
+```no_run
+use std::str::FromStr;
+
+use wruster::handlers;
+use wruster::router;
+use wruster::Server;
+use wruster::http;
+
+env_logger::init();
+let addr = "localhost:8085";
+let routes = router::Router::new();
+let handler: router::HttpHandler = Box::new(move |_| {
+    let greetings = "hello!!";
+    http::Response::from_str(&greetings).unwrap()
+});
+let handler = handlers::log_middleware(handler);
+routes.add("/", http::HttpMethod::GET, handler);
+let mut server = Server::new();
+server.run(addr, routes).unwrap();
+server.wait().unwrap();
+```
+*/
 pub fn log_middleware(handler: HttpHandler) -> HttpHandler {
     Box::new(move |request: Request| {
         info!("request {:?}", request);
